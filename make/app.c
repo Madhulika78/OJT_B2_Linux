73db23ed3ea1d7a0878619445470bbb78f7f6a82
Madhulika78 <127820471+Madhulika78@users.noreply.github.com>
2023-07-06 11:09:31 +0530

    Add files via upload

---
+++ make/app.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<header.h>
+int main(){
+	printf("Sum:%d\n",add(3,4));
+	print();
+}
---
+++ make/dem.sh
@@ -0,0 +1,3 @@
+echo "I am from script file"
+gcc app.c add.c print.c -o appsh
+echo "App generated"
